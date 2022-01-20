cc190aa1ff9188c11a1f1ac23c28ace913379ec7
Eoin Carrick <22carri99@gmail.com>
2022-01-19 21:40:46 -0800

    l

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const body = document.querySelector("body");
 const button = document.querySelector("button");
 
 function changeColor() {
-  const colors = ["red", "blue", "green", "yellow"];
+  const colors = ["red", "blue", "green", "yellow", "#333"];
   let int = Math.floor(Math.random() * colors.length);
   return (body.style.backgroundColor = colors[int]);
 }
